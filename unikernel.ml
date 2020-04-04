53136b5c4900c34acd375673049a60899db7f054
Oliver Flum <ga27pag@tum.de>
2020-04-04 16:06:35 +0200

    Create eval kernel

--- unikernel.ml
+++ unikernel.ml
@@ -1,35 +1,41 @@
 open Lwt.Infix
-
+module StringMap = Map.Make(String)
 module S = Store
 
 module Main (TIME: Mirage_time.S) (PClock: Mirage_clock.PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) = struct
 
   module C = Control.Make (TIME) (PClock)
 
+  (*Data type for adjacency Matrix*)
   type adjacency_rec = {
     atomic_function: string;
     condition: bool;
   }
 
+  (*Custom defined functions*)
   let f1 store =
     let n = S.to_int (store#get "count" (S.VInt 0)) in
-    let loop = function
-    | 200 -> Lwt.return Control.Status.Terminate
-    | n ->
-      if n = 0 
-        then 
-          Logs.info (fun m -> m "HELLO... (%i)" n)
-        else 
-          Logs.info (fun m -> m "AGAIN (%i)" n);
+    if n == 0 then begin
+      Logs.info (fun m -> m "HELLO... (%i)" n);
       store#set "count" (S.VInt (n+1));
       TIME.sleep_ns (Duration.of_sec 1) >>= fun () ->
-    in loop n
-
+      Lwt.return ()
+    end else begin
+      Logs.info (fun m -> m "AGAIN (%i)" n);
+      store#set "count" (S.VInt (n+1));
+      TIME.sleep_ns (Duration.of_sec 1) >>= fun () ->
+      Lwt.return ()
+    end
+  (*The function passed on termination*)
+  let terminate store = 
+    store#terminate
+  
+  (*Define the adjacency matrix here as a associative list*)
   let get_adjacency store =
     let assoc_adj_list = [
       ("f1", [
-        {atomic_function = "f1"; condition = ((S.to_int (store#get "count" (S.VInt 0))) <= 250)};
-        {atomic_function = "terminate"; condition = ((S.to_int (store#get "count" (S.VInt 0))) > 250)};
+        {atomic_function = "f1"; condition = ((S.to_int (store#get "count" (S.VInt 0))) < 200)};
+        {atomic_function = "terminate"; condition = ((S.to_int (store#get "count" (S.VInt 0))) >= 200)};
       ])
     ] in
     let amap = StringMap.of_seq (List.to_seq assoc_adj_list) in
@@ -40,8 +46,39 @@ module Main (TIME: Mirage_time.S) (PClock: Mirage_clock.PCLOCK) (RES: Resolver_l
     let functions = [("f1", f1)] in
     let fmap = StringMap.of_seq (List.to_seq functions) in
     fmap
-  
-  let start _time _pclock res (ctx: CON.t) =
+
+  (*These functions do not need to be edited*)
+  let get_next_function_name store (curr: string) = 
+    let adjacency = get_adjacency store in
+    let adj_arr = StringMap.find curr adjacency in
+    let rec inner = function
+      | [] -> "terminate"
+      | hd::tail -> if hd.condition == true then hd.atomic_function else inner tail
+    in inner adj_arr
+
+  let get_function name =
+    let functions = get_function_map in
+    match name with
+      | "terminate" -> terminate
+      | _ -> StringMap.find name functions
+
+  let rec run store (client: OS.Xs.client) (curr: string) =
+    C.read_shutdown_value client >>= fun status ->
+    Logs.info (fun m -> m "Read control message %s" (Control.Status.string_of_status status));
+    match status with
+      | Control.Status.Resume -> begin
+          let f = get_function curr in
+          f store >>= fun () ->
+          let fnext_name = get_next_function_name store curr in
+          store#set "next" (S.VString fnext_name);
+          run store client fnext_name 
+        end
+      | _ -> begin 
+          let time = C.time () in 
+          store#suspend time status
+        end
+
+  let start _time _pclock resolver conduit =
     let tstr = C.time () in
     Logs.info (fun m -> m "start-TS: %s" tstr);
     let token = Key_gen.token () in
@@ -50,7 +87,7 @@ module Main (TIME: Mirage_time.S) (PClock: Mirage_clock.PCLOCK) (RES: Resolver_l
     let id = Key_gen.id () in
     let host_id = Key_gen.hostid () in
     let store = new S.webStore conduit resolver repo token id host_id in
-    let time = C.time (`) in 
+    let time = C.time () in 
     store#init time migration (C.steady) >>= fun _ ->
     let fct = (S.to_str (store#get "next" (S.VString "f1"))) in
     OS.Xs.make () >>= fun client ->
